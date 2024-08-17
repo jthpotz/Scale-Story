4d1c184ff7c692420c12db385b5121abf5a576a3
Karen Cavicchio <karen.cavicchio@gmail.com>
2024-08-17 13:52:06 -0700

    Added PlayerSprite.cs

--- scripts/Player/PlayerMovement.cs
+++ scripts/Player/PlayerMovement.cs
@@ -14,14 +14,26 @@ public partial class PlayerMovement : Node {
 
 	[Export]
 	private float speed = 1;
+	private bool moving = false;
 
-	public override void _Ready()
-	{
-		animatedSprite.Play("walk");
-	}
 
 	public void SetDirection (Vector2 direction) {
 		this.direction += direction;
+		GD.Print(this.direction);
+		if (this.direction.Length() > 0 && !this.moving) {
+			this.moving = true;
+			animatedSprite.Stop();
+			animatedSprite.Play("walk");
+		} 
+		else if (this.direction.Length() == 0 && this.moving) {
+			this.moving = false;
+			animatedSprite.Stop();
+			animatedSprite.Play("blink");
+		}
+	}
+
+	public bool IsMoving () {
+		return this.direction.Length() > 0;
 	}
 
 	public override void _PhysicsProcess (double delta) {
---
+++ scripts/Player/PlayerSprite.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using Godot;
+using ListExtensions;
+
+public partial class PlayerSprite : AnimatedSprite2D
+{
+    
+    [Export]
+	private PlayerMovement playerMovement;
+
+	public override void _Ready()
+	{
+		Play("walk");
+        AnimationFinished += MyOnAnimationFinished;
+	}
+
+    private void MyOnAnimationFinished() {
+        if (playerMovement.IsMoving()) {
+            Play("walk");
+        }
+        else {
+            var list = new List<string> { "blink", "look-around", "flap-twice" };
+            Play(list.GetRandomItem());
+        }
+    }
+}
