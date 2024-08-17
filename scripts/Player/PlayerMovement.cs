26ae9d813b8b7ca77fb5acd558f2b01e7f638950
jthpotz <jthpotz@gmail.com>
2024-08-17 14:17:10 -0700

    Changed scale and added player facing

--- scripts/Player/PlayerMovement.cs
+++ scripts/Player/PlayerMovement.cs
@@ -4,42 +4,47 @@ using Player;
 
 public partial class PlayerMovement : Node {
 
-	[Export]
-	private PlayerVisual playerVisual;
+    [Export]
+    private PlayerVisual playerVisual;
 
-	private Vector2 direction = new Vector2();
+    private Vector2 direction = new Vector2();
 
-	[Export]
-	private AnimatedSprite2D animatedSprite;
+    private int facing = 1;
 
-	[Export]
-	private float speed = 1;
-	private bool moving = false;
+    [Export]
+    private AnimatedSprite2D animatedSprite;
+
+    [Export]
+    private float speed = 1;
+    private bool moving = false;
 
 
-	public void SetDirection (Vector2 direction) {
-		this.direction += direction;
-		GD.Print(this.direction);
-		if (this.direction.Length() > 0 && !this.moving) {
-			this.moving = true;
-			animatedSprite.Stop();
-			animatedSprite.Play("walk");
-		} 
-		else if (this.direction.Length() == 0 && this.moving) {
-			this.moving = false;
-			animatedSprite.Stop();
-			animatedSprite.Play("blink");
-		}
-	}
+    public void SetDirection (Vector2 direction) {
+        this.direction += direction;
+        if (this.direction.X * facing < 0) {
+            playerVisual.Parent.Scale = new Vector2(playerVisual.Parent.Scale.X * -1, playerVisual.Parent.Scale.Y);
+            facing *= -1;
+        }
+        GD.Print(this.direction);
+        if (this.direction.Length() > 0 && !this.moving) {
+            this.moving = true;
+            animatedSprite.Stop();
+            animatedSprite.Play("walk");
+        } else if (this.direction.Length() == 0 && this.moving) {
+            this.moving = false;
+            animatedSprite.Stop();
+            animatedSprite.Play("blink");
+        }
+    }
 
-	public bool IsMoving () {
-		return this.direction.Length() > 0;
-	}
+    public bool IsMoving () {
+        return this.direction.Length() > 0;
+    }
 
-	public override void _PhysicsProcess (double delta) {
-		((CharacterBody2D)playerVisual.Parent).Velocity = direction * speed;
-		((CharacterBody2D)playerVisual.Parent).MoveAndSlide();
-		
-	}
+    public override void _PhysicsProcess (double delta) {
+        ((CharacterBody2D)playerVisual.Parent).Velocity = direction * speed;
+        ((CharacterBody2D)playerVisual.Parent).MoveAndSlide();
+
+    }
 
 }
