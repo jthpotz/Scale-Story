7b1785d143ba11a998d939e1fa471b6733f84bd8
Karen Cavicchio <karen.cavicchio@gmail.com>
2024-08-16 23:30:07 -0700

    Added animation to player

--- scripts/Player/PlayerMovement.cs
+++ scripts/Player/PlayerMovement.cs
@@ -4,21 +4,30 @@ using Player;
 
 public partial class PlayerMovement : Node {
 
-    [Export]
-    private PlayerVisual playerVisual;
+	[Export]
+	private PlayerVisual playerVisual;
 
-    private Vector2 direction = new Vector2();
+	private Vector2 direction = new Vector2();
 
-    [Export]
-    private float speed = 1;
+	[Export]
+	private AnimatedSprite2D animatedSprite;
 
-    public void SetDirection (Vector2 direction) {
-        this.direction += direction;
-    }
+	[Export]
+	private float speed = 1;
 
-    public override void _PhysicsProcess (double delta) {
-        ((CharacterBody2D)playerVisual.Parent).Velocity = direction * speed;
-        ((CharacterBody2D)playerVisual.Parent).MoveAndSlide();
-    }
+	public override void _Ready()
+	{
+		animatedSprite.Play("walk");
+	}
+
+	public void SetDirection (Vector2 direction) {
+		this.direction += direction;
+	}
+
+	public override void _PhysicsProcess (double delta) {
+		((CharacterBody2D)playerVisual.Parent).Velocity = direction * speed;
+		((CharacterBody2D)playerVisual.Parent).MoveAndSlide();
+		
+	}
 
 }
